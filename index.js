8484c451fc06b6013ff2b6f80c4f3563742fb967
Marcos <marsanben92@gmail.com>
2021-01-30 17:46:37 +0100

    chore: upgrade node-notifier

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.tokenToDate = exports.Token = void 0;
 var models_1 = require("./models");
-exports.Token = models_1.Token;
+Object.defineProperty(exports, "Token", { enumerable: true, get: function () { return models_1.Token; } });
 var utils_1 = require("./utils");
-exports.tokenToDate = utils_1.tokenToDate;
+Object.defineProperty(exports, "tokenToDate", { enumerable: true, get: function () { return utils_1.tokenToDate; } });
